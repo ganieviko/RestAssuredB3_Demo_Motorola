c31c2e534cbaed48f6a653d4f002497f72698d64
daulet2030 <daulet2030@gmail.com>
2021-03-02 08:42:11 +0600

    testing go rest user jackson

--- src/test/java/GoRestUsersTest.java
+++ src/test/java/GoRestUsersTest.java
@@ -3,6 +3,9 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -14,11 +17,17 @@ public class GoRestUsersTest {
 
     @Test
     public void creatingUser() {
+        Map<String, String> body = new HashMap<>();
+        body.put("name", "Techno user");
+        body.put("email", "Floy_OKon13@yahoo.com");
+        body.put("gender", "Female");
+        body.put("status", "Active");
+
         Object id = given()
                 .log().uri()
                 .header("Authorization", "Bearer 55b19d86844d95532f80c9a2103e1a3af0aea11b96817e6a1861b0d6532eef47")
                 .contentType(ContentType.JSON)
-                .body("{\"name\":\"Techno user\",\"email\":\"Floy_OKon12@yahoo.com\",\"gender\":\"Female\",\"status\":\"Active\"}")
+                .body(body)
                 .when()
                 .post()
                 .then()
