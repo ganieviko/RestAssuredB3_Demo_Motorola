2f29f6a0b36df6663b92279fc8547e29f022ba59
daulet2030 <daulet2030@gmail.com>
2021-03-02 06:50:50 +0600

    logging response

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -28,4 +28,14 @@ public class ZippoTest {
                 .then()
         ;
     }
+
+    @Test
+    public void loggingResponse() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().all()
+        ;
+    }
 }
