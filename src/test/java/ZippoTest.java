dbcd8afeb183adaeba8617ba77eec11ccda2e02d
daulet2030 <daulet2030@gmail.com>
2021-03-02 06:25:26 +0600

    status code test

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,8 +1,20 @@
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
     @Test
     public void test() {
+        given().when().then();
+    }
 
+    @Test
+    public void testingStatusCode() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .statusCode(200)
+        ;
     }
 }
