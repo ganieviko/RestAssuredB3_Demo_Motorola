0056b8ddd960df10ecbba335d84b55078c8a1a91
daulet2030 <daulet2030@gmail.com>
2021-03-03 06:32:55 +0600

    testing go rest response specs

--- src/test/java/GoRestUsersTest.java
+++ src/test/java/GoRestUsersTest.java
@@ -1,6 +1,8 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -30,6 +32,11 @@ public class GoRestUsersTest {
                 .header("Authorization", "Bearer 55b19d86844d95532f80c9a2103e1a3af0aea11b96817e6a1861b0d6532eef47")
                 .contentType(ContentType.JSON);
 
+        ResponseSpecification defaultTestsForResponse = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+
         Object id = given()
                 .spec(givenReqSpec)
                 .body(body)
@@ -37,6 +44,7 @@ public class GoRestUsersTest {
                 .post()
                 .then()
                 .log().body()
+                .spec(defaultTestsForResponse)
                 .body("code", equalTo(201))
                 .extract().path("data.id");
 
@@ -48,6 +56,7 @@ public class GoRestUsersTest {
                 .post()
                 .then()
                 .log().body()
+                .spec(defaultTestsForResponse)
                 .body("code", equalTo(422));
 
         given()
@@ -55,6 +64,7 @@ public class GoRestUsersTest {
                 .get("/" + id)
                 .then()
                 .log().body()
+                .spec(defaultTestsForResponse)
                 .body("data.name", equalTo(body.get("name")))
                 .body("data.email", equalTo(body.get("email")))
                 .body("data.gender", equalTo(body.get("gender")))
